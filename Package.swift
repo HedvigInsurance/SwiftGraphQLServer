a007fc0298c7f6bc3ff3300babf823c4158169b0
Sam Pettersson <sam@sampettersson.com>
2019-04-28 18:26:25 +0200

    Swift 5

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
             targets: ["SwiftGraphQLServer"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/GraphQLSwift/Graphiti.git", from: "0.8.0"),
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0")
+        .package(url: "https://github.com/HedvigInsurance/Graphiti.git", from: "0.9.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.3.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
