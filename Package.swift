989c11d4ba55c82940797618c9ddb7a6d250fdce
Sam Pettersson <sam@sampettersson.com>
2019-06-15 10:49:59 +0200

    Bump deps

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
             targets: ["SwiftGraphQLServer"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/HedvigInsurance/Graphiti.git", from: "0.13.0"),
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.3.0")
+        .package(url: "https://github.com/HedvigInsurance/Graphiti.git", .exact("0.14.0")),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-alpha")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
